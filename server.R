62e06f3c933b078dbbc8a1bdd81e92b82bb6eddb
Hannah Lee <hanss21@uw.edu>
2018-03-05 20:07:47 -0800

    code for single widget

--- server.R
+++ server.R
@@ -5,47 +5,27 @@ library(maps)
 library(mapproj)
 
 
-
 server <- function(input, output) {
-  
- 
-  d <- reactive({
+  #d <- reactive({
     my.data <- read.csv('data/globalterrorism.csv', 
                         stringsAsFactors=FALSE)
-    my.data <- my.data %>% na.omit()
+    my.data <- my.data %>% na.omit() 
+  #})
+  
+    h.data <- select(my.data, iyear, country_txt, region, latitude, longitude)  %>% 
+      filter(country_txt != "United States") %>%
+      unique()
 
-  })
-  
-  # overview server code
-  output$overview <- renderText({
-    paste("overview of project, citations, etc.")
     
-  })
-  
-  # justins server code
-  output$justin <- renderText({
-    paste("justin- all of the things")
-    
-  })
-  
-  # zales server code
-  output$zale <- renderText({
-    paste("zale- all of things")
-    
-  })
+  #vector <- reactive({
+    vector.country.names <- h.data[, "country_txt"] 
+  #})
   
   # hannahs server code
-  output$hannah <- renderText({
-    paste("hannah- all the things")
-    
+  output$comparisonPlot <- renderPlot({
+ 
   })
-  
-  # madys server code
-  output$mady <- renderText({
-    paste("mady- all the things")
-    
-  })
-  
+
 }
 
 shinyServer(server)
