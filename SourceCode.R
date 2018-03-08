190c9253b8eae5e9e5a95afec7dae31b8eb4e7c6
Zale de Jong <zaled@uw.edo>
2018-03-08 10:22:21 -0800

    Add finalized version of project

--- SourceCode.R
+++ SourceCode.R
@@ -15,7 +15,7 @@ library(mapproj)
 
 # Reads in the data set "globalterrorism.csv" and stores it in 
 # the variable my.data.
-my.data <- read.csv('./data/globalterrorism.csv', 
+my.data <- read.csv('./data/globalterrorism.csv',
                     stringsAsFactors=FALSE)
 my.data <- arrange(my.data, country_txt)
 
@@ -28,7 +28,7 @@ hl.q5.data <- select(my.data, iyear, country_txt)  %>%
 hannah.vector <- sort(hl.q5.data[, "country_txt"]) 
 
 
-#
+# Creates a copy of the data frame for Zale.
 zale.data <- my.data
 
 # Renames the first and seventh columns.
@@ -44,26 +44,8 @@ zale.data.filtered <- zale.data %>%
 # Renames the second column of the data frame.
 colnames(zale.data.filtered)[2] <- "Country"
 
-#
-zale.countries <- as.data.frame(sort(unique(zale.data.filtered$Country)), stringsAsFactors = FALSE)
-colnames(zale.countries)[1] <- "Country"
+# Creates a vector of types of attacks.
 attack.type <- as.vector(sort(unique(zale.data.filtered$attack_type)))
 
 # Stores the range of years the data frame spans.
 zale.year.range <- c(1970:2016)
-
-# Stores the world map data frame and removes the column subregion.
-zale.world.map <- map_data("world") %>%
-  select(-subregion)
-# Renames the fifth column of the data frame.
-colnames(zale.world.map)[5] <- "Country"
-
-# Adds a new column of two digit ISO codes to each country.
-zale.world.map$a2 <- iso.alpha(zale.world.map$Country, n = 2)
-
-# Stores the iso3166 data frame in a new data frame.
-iso.data.frame <- iso3166
-# Renames the third row of the data frame.
-colnames(iso.data.frame)[3] <- "Country"
-
-
--- server.R
+++ server.R
@@ -11,21 +11,21 @@ server <- function(input, output) {
   # reactive element that changes the data set everytime the widgets are changed
   d <- reactive({
     current.unit <- rlang::sym(input$unit)
-    
+
     # filter data according to inputs in widgets
     the.data <- filter(my.data, iyear <= input$endyear) %>%
       filter(iyear >= input$startyear) %>%
       group_by(!!current.unit) %>%
       summarize(count = n())
-    
+
   })
-  
+
   # overview server code
   output$overview <- renderText({
     paste("overview of project, citations, etc.")
-    
+
   })
-  
+
   # justins server code
   user.data <- reactive({
     country.data <- my.data %>%
@@ -33,9 +33,9 @@ server <- function(input, output) {
       filter(iyear == input$year)
     return(country.data)
   })
-  
+
   output$justin <- renderPlot({
-    
+
     ggplot(data = user.data()) +
       geom_bar(mapping = aes(x = country_txt, fill = country_txt),
                show.legend = FALSE) +
@@ -44,24 +44,24 @@ server <- function(input, output) {
         y = "Terrorist Attacks"
       )
   })
-  
+
   #
   output$justin.text <- renderText({
-    print("It can be seen that in the year 1970,  the United States had an 
+    print("It can be seen that in the year 1970,  the United States had an
           unusually large amount of terrorism. Other countries with large
-          amounts of terrorism include Argentina, Spain, and Uruguay. However, 
-          the United States drastically overtakes these countries with a huge 
-          500+ terrorist attacks in 1970. It seems that these countries, 
-          especially the US, are the most prone to terrorism. In 1972, however, 
-          the amount of terrorism drastically decreases with the amount in the 
-          US being less than 100 attacks. Then in 1973 the amount of attacks in 
-          Argentina surpasses the US. In 1977 the amount of terrorism in Spain 
+          amounts of terrorism include Argentina, Spain, and Uruguay. However,
+          the United States drastically overtakes these countries with a huge
+          500+ terrorist attacks in 1970. It seems that these countries,
+          especially the US, are the most prone to terrorism. In 1972, however,
+          the amount of terrorism drastically decreases with the amount in the
+          US being less than 100 attacks. Then in 1973 the amount of attacks in
+          Argentina surpasses the US. In 1977 the amount of terrorism in Spain
           skyrockets and continues to increase until 1980 where it makes a slow
           decline, but increases again in 1982. Spain stays as the country with
-          the most terrorism all this time up to 1994, where the US takes the 
-          top spot again. At this point in time Russia makes a huge increase in 
-          terrorist attacks in their country. From then on until 2016, 
-          terrorism has declined in all 4 of these countries, however, in 2003, 
+          the most terrorism all this time up to 1994, where the US takes the
+          top spot again. At this point in time Russia makes a huge increase in
+          terrorist attacks in their country. From then on until 2016,
+          terrorism has declined in all 4 of these countries, however, in 2003,
           Iraq gains a huge increase in terrorist attacks. The amount of attacks
           on Iraq keep increasing all the way through 2016. Although Argentina,
           Iraq, Russia, Spain, the United States, and Uruguay are generally more
@@ -69,143 +69,10 @@ server <- function(input, output) {
           be chosen to be compared over time.")
   })
   
-  output$map <- renderPlot({
-    # Creates a data frame of the year span and attack types input
-    # by the user.
-    zale.data.formatted <- zale.data.filtered  %>%
-      filter(year >= input$year[1], year <= input$year[2],
-             attack_type %in% input$attack_type) %>%
-      with(table(Country, attack_type))
-    zale.data.formatted <- as.data.frame(zale.data.formatted)
-    # Creates a data frame of the number of occurrences of terrorism
-    # without some countries.
-    zale.data.occurrences.missing <- zale.data.formatted %>%
-      group_by(Country) %>%
-      summarise(Occurrences = sum(Freq)) %>%
-      ungroup()
-    zale.data.occurrences.missing$Country <- as.character(
-                                            zale.data.occurrences.missing$Country)
-    # Creates a data frame with every country, and 0 for no
-    # occurances.
-    zale.data.occurrences <- zale.data.occurrences.missing %>%
-      right_join(zale.countries, by = "Country") %>%
-      mutate_all(funs(replace(., is.na(.), 0)))
-    
-    # Renames old countries which can be assigned to new countries.
-    zale.data.occurrences[21, 1] <- "Bolivia, Plurinational State of"
-    zale.data.occurrences[22, 1] <- "Bosnia and Herzegovina"
-    zale.data.occurrences[25, 1] <- "Brunei Darussalam"
-    zale.data.occurrences[50, 1] <- "Timor-Leste"
-    zale.data.occurrences[58, 1] <- "Falkland Islands (Malvinas)"
-    zale.data.occurrences[84, 1] <- "Iran, Islamic Republic of"
-    zale.data.occurrences[89, 1] <- "Cote d'Ivoire"
-    zale.data.occurrences[98, 1] <- "Lao People's Democratic Republic"
-    zale.data.occurrences[106, 1] <- "Macao"
-    zale.data.occurrences[107, 1] <- "the former Yugoslav Republic of Macedonia"
-    zale.data.occurrences[118, 1] <- "Moldova, Republic of"
-    zale.data.occurrences[132, 1] <- "Democratic People's Republic of Korea"
-    zale.data.occurrences[157, 1] <- "Slovakia"
-    zale.data.occurrences[162, 1] <- "Republic of Korea"
-    zale.data.occurrences[169, 1] <- "Saint Kitts and Newis"
-    zale.data.occurrences[170, 1] <- "Saint Lucia"
-    zale.data.occurrences[176, 1] <- "Syrian Arab Republic"
-    zale.data.occurrences[179, 1] <- "Tanzania, United Republic of"
-    zale.data.occurrences[189, 1] <-
-                          "United Kingdom of Great Britain and Northern Ireland"
-    zale.data.occurrences[194, 1] <- "Holy See (Vatican City State)"
-    zale.data.occurrences[198, 1] <- "Palestine, State of"
-    zale.data.occurrences[203, 1] <- "Democratic Republic of the Congo"
-    
-    # Creates new values in the data frame of new countries which didn't exist
-    # then.
-    zale.data.occurrences[206, 1] <- "Czech Republic"
-    zale.data.occurrences[206, 2] <- zale.data.occurrences[42, 2] +
-                                     zale.data.occurrences[43, 2]
-    zale.data.occurrences[207, 1] <- "Germany"
-    zale.data.occurrences[207, 2] <- zale.data.occurrences[49, 2] +
-                                     zale.data.occurrences[67, 2] + 
-                                     zale.data.occurrences[199, 2]
-    zale.data.occurrences[208, 1] <- "Vanuatu"
-    zale.data.occurrences[208, 2] <- zale.data.occurrences[127, 2] +
-                                     zale.data.occurrences[193, 2]
-    zale.data.occurrences[209, 1] <- "Yemen"
-    zale.data.occurrences[209, 2] <- zale.data.occurrences[133, 2] +
-                                     zale.data.occurrences[165, 2]
-    zale.data.occurrences[210, 1] <- "Republic of Congo"
-    zale.data.occurrences[210, 2] <- zale.data.occurrences[139, 2] +
-                                     zale.data.occurrences[145, 2]
-    zale.data.occurrences[211, 1] <- "Zimbabwe"
-    zale.data.occurrences[211, 2] <- zale.data.occurrences[146, 2] +
-                                     zale.data.occurrences[205, 2]
-    zale.data.occurrences[212, 1] <- "Russian Federation"
-    zale.data.occurrences[212, 2] <- zale.data.occurrences[148, 2] +
-                                     zale.data.occurrences[166, 2]
-    zale.data.occurrences[213, 1] <- "Serbia"
-    zale.data.occurrences[213, 2] <- zale.data.occurrences[153, 2] +
-                                     zale.data.occurrences[152, 2] +
-                                     zale.data.occurrences[202, 2]
-    zale.data.occurrences[214, 1] <- "Vietnam"
-    zale.data.occurrences[214, 2] <- zale.data.occurrences[164, 2] +
-                                     zale.data.occurrences[196, 2]
-
-    # Joins the zale.data.occurrences data frame with the iso.data.frame,
-    # selects the desired columns, and omits the NA values.
-    zale.data.iso <- zale.data.occurrences %>%
-      left_join(iso.data.frame, by = "Country") %>%
-      select(Country, Occurrences, a2) %>%
-      na.omit()
-    
-    # Joins the zale.data.iso and zale.world.map data frames.
-    zale.data.all <- zale.data.iso %>%
-      left_join(zale.world.map, by = "a2")
-    
-    # Renames the first column.
-    colnames(zale.data.all)[1] <- "Country"
-    
-    # Selects the desired columns.
-    zale.data.final <- zale.data.all %>%
-      select(Country, Occurrences, long, lat, group)
-    
-    
-    # Breaks the frequency of attacks.
-    zale.data.final$breaks <- cut(zale.data.final$Occurrences,
-                                  breaks = c(0, 500, 1000, 1500,
-                                             2000, 2500, 3000, 3500),
-                                  labels = c("0 - 500",
-                                             "500 - 1000",
-                                             "1000 - 1500",
-                                             "1500 - 2000",
-                                             "2000 - 2500",
-                                             "2500 - 3000",
-                                             "3000 - 3500")
-    )
-    
-    # Returns a ggplot polygon world map for the terrorism dataset,
-    # with longitude on the x and latitude on the y axis.
-    ggplot(data = zale.data.final) +
-      geom_polygon(aes(x = long, y = lat, group = group, fill = breaks),
-                   color = "black") +
-      coord_quickmap() +
-      scale_fill_brewer(palette = "Greens") +
-      labs(title = "World Map",
-           x = "Longitude",
-           y = "Latitude",
-           fill = "Number Of Attacks")
-  })
-  
-  # Renders the map description output.
-  output$zale_map_description <- renderText({
-    paste("The map above displays the frequency of terrorist attacks in
-          various locations around the world for any given span of
-          years from 1970 to 2016. As you interact with the slider,
-          try and look for countries that either change drastically
-          in color, or stay a consistently light or dark color.")
-  })
-  
   # Renders a DataTable with the correct information.
   output$table <- renderDataTable({
     zale.data.formatted <- zale.data.filtered  %>%
-      filter(year >= input$year[1], year <= input$year[2]) %>%
+      filter(year >= input$zale_year[1], year <= input$zale_year[2]) %>%
       with(table(Country, attack_type))
     
     zale.data.formatted <- as.data.frame(zale.data.formatted)
@@ -226,7 +93,7 @@ server <- function(input, output) {
 
   # Mady's server code
   output$alsomady <- renderPlot({
-    
+
     # shows year unit chart
     if (input$unit == "iyear") {
       print("year")
@@ -236,8 +103,8 @@ server <- function(input, output) {
             x = "year",
             y = "count"
           ))
-    } 
-    
+    }
+
     # Shows month unit chart
     if (input$unit == "imonth") {
       print("month")
@@ -249,7 +116,7 @@ server <- function(input, output) {
             y = "count"
           ))
     }
-    
+
     # Shows day unit chart
     if (input$unit == "iday") {
       print("iday")
@@ -261,12 +128,12 @@ server <- function(input, output) {
           ))
     }
   })
-  
+
   # Creates a paragraph of text explaining the chart
   output$mady <- renderText({
     paste("This line plot shows the frequency of terrorist attacks
           over time. The frequency can be by year, month of the year, or day of the month. What
-          unit is shown can be selected using the radio buttons on the left. The 
+          unit is shown can be selected using the radio buttons on the left. The
           plot can also show the data over a certain range of years, using the sliders
           on the left.")
   })
@@ -274,44 +141,44 @@ server <- function(input, output) {
 
   # Creates another paragraph of text explaining the chart
   output$anothermady <- renderText({
-    paste("The plot above shows that while the frequency in terrorist attacks do not change much by month of the 
-          year or day of the month. However, the frequency of terrorist attck do change a lot by year. The plot 
+    paste("The plot above shows that while the frequency in terrorist attacks do not change much by month of the
+          year or day of the month. However, the frequency of terrorist attck do change a lot by year. The plot
           shows that the number of terrorist attacks spiked dramatically after 2010.")
   })
-  # Creates a dataframe of the United States and the input$country 
+  # Creates a dataframe of the United States and the input$country
   # then it filters it by the input$year
     hl.country <- reactive({
-      new.data.frame <- filter(my.data, country_txt == "United States") %>% 
+      new.data.frame <- filter(my.data, country_txt == "United States") %>%
         filter(iyear == input$years)
       new.one <- filter(my.data, country_txt == input$country) %>%
         filter(iyear == input$years)
       combine <- full_join(new.data.frame, new.one)
-    }) 
-    
-    # Creates the dataframe that will be used find the number of times 
-    # United States and the input$country occurs 
+    })
+
+    # Creates the dataframe that will be used find the number of times
+    # United States and the input$country occurs
     h.code <- reactive ({
-      count.united.states <- select(hl.country(), country_txt) %>% count(country_txt == "United States")  
-      # gets the number of times United States is used in the dataframe 
+      count.united.states <- select(hl.country(), country_txt) %>% count(country_txt == "United States")
+      # gets the number of times United States is used in the dataframe
       new <- count.united.states[,2:2]
       new.num <- new[-1,] # grabs the number of occurances of that dataframe
       new.num$name <- "United States"
-      
-      count.input <- select(hl.country(), country_txt) %>% count(country_txt == input$country)  
-      input.count <- count.input[,2:2] 
+
+      count.input <- select(hl.country(), country_txt) %>% count(country_txt == input$country)
+      input.count <- count.input[,2:2]
       input.num <- input.count[-1,] # grabs the number of occurances of that dataframe
       input.num$name <- input$country
-      
+
       combine.data <- full_join(input.num, new.num)
       combine.data
     })
-    
-    # Renders the bar plot 
+
+    # Renders the bar plot
     output$bar <- renderPlot({
-      return(ggplot(data = h.code(), aes(x = name, y = n, fill = name), show.legend = FALSE) + 
+      return(ggplot(data = h.code(), aes(x = name, y = n, fill = name), show.legend = FALSE) +
                geom_bar(stat = "identity"))
     })
-    
+
 } 
 
 shinyServer(server)
