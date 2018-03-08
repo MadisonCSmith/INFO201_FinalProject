f2cc3f05333cebce98d095202e6efd96c04f6f87
Hannah Lee <hanss21@uw.edu>
2018-03-07 18:18:09 -0800

    completed sections for part5

--- server.R
+++ server.R
@@ -1,31 +1,53 @@
-library("shiny")
+library(shiny) 
 library(dplyr)
-library("ggplot2")
+library(ggplot2)
 library(maps)
 library(mapproj)
 
 
 server <- function(input, output) {
-  #d <- reactive({
-    my.data <- read.csv('data/globalterrorism.csv', 
-                        stringsAsFactors=FALSE)
-    my.data <- my.data %>% na.omit() 
-  #})
+    my.data <- read.csv('./data/globalterrorism.csv', 
+                        stringsAsFactors=FALSE) 
+    #my.data <- my.data %>% na.omit() 
   
-    h.data <- select(my.data, iyear, country_txt, region, latitude, longitude)  %>% 
+    # New data frame that has the year, country name, region, lat, long will get rid of the United States 
+    # and only keep one of each 
+    hl.q5.data <- select(my.data, iyear, country_txt, region, latitude, longitude)  %>% 
       filter(country_txt != "United States") %>%
-      unique()
-
+      unique() 
     
-  #vector <- reactive({
-    vector.country.names <- h.data[, "country_txt"] 
-  #})
-  
-  # hannahs server code
-  output$comparisonPlot <- renderPlot({
- 
-  })
-
-}
+    # This creates a vecotr of country names 
+    hannah.vector <- sort(q5.data[, "country_txt"]) 
+    
+    hl.country <- reactive({
+      new.data.frame <- filter(my.data, country_txt == "United States") %>% 
+        filter(iyear == input$years)
+      new.one <- filter(my.data, country_txt == input$country) %>%
+        filter(iyear == input$years)
+      combine <- full_join(new.data.frame, new.one)
+    })
+    
+    h.code <- reactive ({
+      count.united.states <- select(hl.country(), country_txt) %>% count(country_txt == "United States")  
+      # gets the number of times United States is used in the dataframe 
+      new <- count.united.states[,2:2]
+      new.num <- new[-1,] # grabs the number of occurances of that dataframe
+      new.num$name <- "United States"
+      
+      count.input <- select(hl.country(), country_txt) %>% count(country_txt == input$country)  
+      input.count <- count.input[,2:2] 
+      input.num <- input.count[-1,] # grabs the number of occurances of that dataframe
+      input.num$name <- input$country
+      
+      combine.data <- full_join(input.num, new.num)
+      combine.data
+    })
+    
+    output$bar <- renderPlot({
+      return(ggplot(data = h.code(), aes(x = name, y = n, fill = name), show.legend = FALSE) + 
+               geom_bar(stat = "identity"))
+    })
+    
+} 
 
 shinyServer(server)
--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
-library("shiny")
+library(shiny)
 library(maps)
 library(mapproj)
+library(rsconnect)
 
 
 ui <- navbarPage("Global Terrorism",
@@ -13,14 +14,27 @@ ui <- navbarPage("Global Terrorism",
 
   # all hannah's ui code
   tabPanel("Hannah",
+    titlePanel("Is the United States at a significantly higher risk for terrorist attacks than other
+            countries?"),
     sidebarLayout(
       sidebarPanel(
         selectInput('country', "Select the country you would like to compare to",
-                    choice =  vector.country.names)
+                    choice = hannah.vector),
+        sliderInput("years", "Select the desired year", 
+                    min=1970, max=2016, value = 1991, step = 1)
         ),
              
-      mainPanel(
-        textOutput("hannah")
+      mainPanel( 
+        br(),
+        p("The bar plot below will answer the question above depending on the country
+          and year that is picked. The bar graph will always show the United States data 
+          however, the compared country will change."),
+        plotOutput("bar"),
+        br(),
+        p("As you can see, the data is always change according to the year and country. 
+          It is interesting to see the frequency of terrorist attack across country and year 
+          compared to the United States. There are trends of terrorist attack depending on 
+          year and country that reflect the state of country. ")
       )
     )
   )
